e33d2fd2cd2695fcdca2b93482b3049062db7d3e
NamanBansal9 <namanbansal003@gmail.com>
2025-08-24 12:59:08 +0530

    2 ques modified

--- assiqn2/q2.cpp
+++ assiqn2/q2.cpp
@@ -5,12 +5,17 @@ int main(){
     int arr[7] = {64,34,25,12,22,11,90};
     int n;
     for(int i = 0;i<7;i++){
+        int c = 1;
         for(int j = 0;j<(7-i);j++){
             if(arr[j] > arr[j+1]){
                 n = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = n;
+                c = 0;
             }
+        }
+        if(c == 1){
+            break;
         }
     }
     for(int i=0;i<7;i++){
